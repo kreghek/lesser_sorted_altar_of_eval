c1edb3ae7e3f3457396737d57bc9394543c91eb7
Куркутов Павел <kurkutovpa@tomskasu.ru>
2018-02-07 22:22:01 +0700

    Разработка сортировки кучей - поддержка основных свойств кучи.

---
+++ Algo/Sorters/HeapSorter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Algo.Sorters
+{
+    public class HeapSorter : ISorter
+    {
+        public int[] Sort(int[] source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Heapify(Heap heap, int i)
+        {
+            var left = Left(i);
+            var right = Right(i);
+            var largest = 0;
+
+            if (left <= heap.HeapSize - 1 && heap.A[left] > heap.A[i])
+            {
+                largest = left;
+            }
+            else
+            {
+                largest = i;
+            }
+
+            if (right <= heap.HeapSize - 1 && heap.A[right] > heap.A[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                Change(heap.A, i, largest);
+                Heapify(heap, largest);
+            }
+        }
+
+        public void BuildHeap(Heap heap)
+        {
+            heap.HeapSize = heap.A.Length;
+
+            for (var i = heap.A.Length / 2; i >= 0; i--)
+            {
+                Heapify(heap, i);
+            }
+        }
+
+        public void HeapSort(Heap heap)
+        {
+            BuildHeap(heap);
+            for (var i = heap.A.Length; i >= 1; i--)
+            {
+                Change(heap.A, 0, i);
+                heap.HeapSize--;
+                Heapify(heap, 0);
+            }
+        }
+
+        public int Parent(int i)
+        {
+            return (i + 1) / 2;
+        }
+
+        public int Left(int i)
+        {
+            //TODO Можно заменить на операции левого и правого сдвига
+            return 2 * (i + 1) - 1;
+        }
+
+        public int Right(int i)
+        {
+            return 2 * (i + 1);
+        }
+
+        public void Change(int[] a, int x, int y)
+        {
+            var temp = a[x];
+            a[x] = a[y];
+            a[y] = temp;
+        }
+    }
+
+    public class Heap
+    {
+        public int HeapSize { get; set; }
+        public int[] A { get; set; }
+    }
+}
---
+++ Tests/HeapSorterTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Algo.Sorters;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class HeapSorterTests
+    {
+        /// <summary>
+        /// 1. В системе есть массив из 3 элементов, удовлетворяющий основному свойству кучи (A[Parent(i)]>A[i]).
+        /// 2. Поддерживаем кучу.
+        /// 3. Куча не изменилась.
+        /// </summary>
+        [Test]
+        public void Heapify_HasHeapProperty_ReturnsSameHeap()
+        {
+            //ARRANGE
+
+            var sorter = new HeapSorter();
+            var a = new[] { 6, 3, 1 };
+            var heap = new Heap { A = a, HeapSize = a.Length };
+
+
+            // ACT
+
+            sorter.Heapify(heap, 0);
+
+
+
+            //ASSERT
+
+            AssertArrayIsHeap(a, 0);
+        }
+
+        /// <summary>
+        /// 1. В системе есть массив из 3 элементов. Не поддерживает свойство кучи (левый потомок больше родителя).
+        /// 2. Поддерживаем кучу.
+        /// 3. Куча стала поддерживать свойтсво кучи.
+        /// </summary>
+        [Test]
+        public void Heapify_LeftIsGreater_ReturnsHeapProperty()
+        {
+            //ARRANGE
+
+            var sorter = new HeapSorter();
+            var a = new[] { 3, 5, 1 };
+            var heap = new Heap { A = a, HeapSize = a.Length };
+
+
+            // ACT
+
+            sorter.Heapify(heap, 0);
+
+
+
+            //ASSERT
+
+            AssertArrayIsHeap(a, 0);
+        }
+
+        /// <summary>
+        /// 1. В системе есть массив из 3 элементов. Не поддерживает свойство кучи (правый потомок больше родителя).
+        /// 2. Поддерживаем кучу.
+        /// 3. Куча стала поддерживать свойтсво кучи.
+        /// </summary>
+        [Test]
+        public void Heapify_RightIsGreater_ReturnsHeapProperty()
+        {
+            //ARRANGE
+
+            var sorter = new HeapSorter();
+            var a = new[] { 3, 1, 5 };
+            var heap = new Heap { A = a, HeapSize = a.Length };
+
+
+            // ACT
+
+            sorter.Heapify(heap, 0);
+
+
+
+            //ASSERT
+
+            AssertArrayIsHeap(a, 0);
+        }
+
+        /// <summary>
+        /// См формулу 7.1
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="parent"></param>
+        private void AssertArrayIsHeap(int[] a, int parent)
+        {
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (parent == i)
+                    continue;
+
+                Assert.GreaterOrEqual(a[parent], a[i]);
+            }
+        }
+    }
+}
